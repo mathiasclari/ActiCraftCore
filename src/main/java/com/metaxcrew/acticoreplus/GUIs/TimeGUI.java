92c3625d90780d3d108fa475ed794015f4a81e47
Mat <silencekrakens@gmail.com>
2022-02-23 23:11:04 +0100

    ParkChat

--- src/main/java/com/metaxcrew/acticoreplus/GUIs/TimeGUI.java
+++ src/main/java/com/metaxcrew/acticoreplus/GUIs/TimeGUI.java
@@ -32,24 +32,39 @@ public class TimeGUI implements CommandExecutor, Listener {
                 Inventory TIMESGUI = Bukkit.createInventory(null, 9, ChatColor.of(new Color(69, 72, 77)) + "Time Selector");
 
                 // SetItems
-                ItemStack day = new ItemStack(Material.YELLOW_STAINED_GLASS, 1);
+                ItemStack morning = new ItemStack(Material.YELLOW_STAINED_GLASS, 1);
+                ItemStack  day = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS, 1);
+                ItemStack midnight = new ItemStack(Material.BLUE_STAINED_GLASS, 1);
+                ItemStack night = new ItemStack(Material.ORANGE_STAINED_GLASS, 1);
 
-                ItemStack night = new ItemStack(Material.BLUE_STAINED_GLASS, 1);
 
+                ItemMeta morning_meta = morning.getItemMeta();
+                morning_meta.setDisplayName(ChatColor.of(new Color(241, 152, 35)) + "MORNING");
+                ArrayList<String> morning_lore = new ArrayList<>();
+                morning_lore.add(ChatColor.of(new Color(130, 139, 147)) + "Change world time to morning!");
+                morning_meta.setLore(morning_lore);
+                morning.setItemMeta(morning_meta);
 
                 ItemMeta day_meta = day.getItemMeta();
-                day_meta.setDisplayName(ChatColor.of(new Color(241, 152, 35)) + "DAY");
+                day_meta.setDisplayName(ChatColor.of(new Color(241, 152, 35)) + "Day");
                 ArrayList<String> day_lore = new ArrayList<>();
                 day_lore.add(ChatColor.of(new Color(130, 139, 147)) + "Change world time to day!");
                 day_meta.setLore(day_lore);
                 day.setItemMeta(day_meta);
 
                 ItemMeta night_meta = night.getItemMeta();
-                night_meta.setDisplayName(ChatColor.of(new Color(54, 45, 179)) + "MIDNIGHT");
+                night_meta.setDisplayName(ChatColor.of(new Color(54, 45, 179)) + "NIGHT");
                 ArrayList<String> night_lore = new ArrayList<>();
                 night_lore.add(ChatColor.of(new Color(130, 139, 147)) + "Change world time to midnight!");
                 night_meta.setLore(night_lore);
                 night.setItemMeta(night_meta);
+
+                ItemMeta midnight_meta = midnight.getItemMeta();
+                night_meta.setDisplayName(ChatColor.of(new Color(54, 45, 179)) + "MIDNIGHT");
+                ArrayList<String> midnight_lore = new ArrayList<>();
+                midnight_lore.add(ChatColor.of(new Color(130, 139, 147)) + "Change world time to midnight!");
+                midnight_meta.setLore(midnight_lore);
+                midnight.setItemMeta(midnight_meta);
 
 
                 String money = "%vault_eco_balance_fixed%";
@@ -77,8 +92,10 @@ public class TimeGUI implements CommandExecutor, Listener {
             language.setItemMeta(langhead);*/
 
                 //Design
-                TIMESGUI.setItem(3, day);
-                TIMESGUI.setItem(5, night);
+                TIMESGUI.setItem(2, morning);
+                TIMESGUI.setItem(6, night);
+                TIMESGUI.setItem(4, day);
+                TIMESGUI.setItem(8, midnight);
 
                 player.openInventory(TIMESGUI);
 
@@ -99,6 +116,28 @@ public class TimeGUI implements CommandExecutor, Listener {
                 if (event.getClick().equals(ClickType.RIGHT) || event.getClick().equals(ClickType.LEFT)) {
                     ItemMeta meta = event.getCurrentItem().getItemMeta();
                     event.getWhoClicked().getWorld().setTime(0);
+                    event.getWhoClicked().sendMessage(ChatColor.of(new Color(130, 139, 147)) + "You changed world time to: " + ChatColor.of(new Color(0, 141, 183)) + "MORNING");
+                    event.getWhoClicked().closeInventory();
+                } else {
+                    event.setCancelled(true);
+                }
+            }
+
+            if (event.getCurrentItem().getType() == Material.ORANGE_STAINED_GLASS) {
+                if (event.getClick().equals(ClickType.RIGHT) || event.getClick().equals(ClickType.LEFT)) {
+                    ItemMeta meta = event.getCurrentItem().getItemMeta();
+                    event.getWhoClicked().getWorld().setTime(12000);
+                    event.getWhoClicked().sendMessage(ChatColor.of(new Color(130, 139, 147)) + "You changed world time to: " + ChatColor.of(new Color(0, 141, 183)) + "SUNSET");
+                    event.getWhoClicked().closeInventory();
+                } else {
+                    event.setCancelled(true);
+                }
+            }
+
+            if (event.getCurrentItem().getType() == Material.LIGHT_BLUE_STAINED_GLASS) {
+                if (event.getClick().equals(ClickType.RIGHT) || event.getClick().equals(ClickType.LEFT)) {
+                    ItemMeta meta = event.getCurrentItem().getItemMeta();
+                    event.getWhoClicked().getWorld().setTime(1000);
                     event.getWhoClicked().sendMessage(ChatColor.of(new Color(130, 139, 147)) + "You changed world time to: " + ChatColor.of(new Color(0, 141, 183)) + "DAY");
                     event.getWhoClicked().closeInventory();
                 } else {
